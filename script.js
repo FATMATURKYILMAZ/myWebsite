7cf1199a67caf985a21d81545f0d528ac86066fd
fatma_turkyilmaz <96384069+FATMATURKYILMAZ@users.noreply.github.com>
2023-04-10 18:36:34 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,32 @@ window.onscroll=()=>{
 let header=document.querySelector('.header');
 header.classList.toggle('sticky', window.scrollY>100);
 }
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween:50,
+    loop:true,
+    grabCursor:true,
+    slidesPerGroupSkip: 1,
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+    },
+    breakpoints: {
+      769: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+      },
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
